a0e98783287d099865929de7e967daa3b15a0be1
nemoto <haikonggenben@gmail.com>
2025-11-13 23:58:08 +0900

    save
    iroirokaeta

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 const modeBtn = document.getElementById("mode-toggle");
 const body = document.body;
+const slides = document.querySelectorAll(".slides img");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
 const profileImg = document.getElementById("profile-img");
-const originalSrc = "images/profile-img.jpg";
+const originalSrc = "/img/profile-image.png";
 const hoverSrc = "/img/chickens.png";
+const menuIcon = document.getElementById("menu-icon");
+const nav = document.getElementById("nav");
 
 modeBtn.addEventListener("click", () => {
   body.classList.toggle("dark-mode");
@@ -14,6 +19,12 @@ modeBtn.addEventListener("click", () => {
     modeBtn.textContent = "🌙";
   }
 });
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("active"); // アニメーション切り替え
+  nav.classList.toggle("active"); // メニュー開閉
+});
+
 // マウスが乗ったとき
 profileImg.addEventListener("mouseenter", () => {
   profileImg.src = hoverSrc;
@@ -23,3 +34,50 @@ profileImg.addEventListener("mouseenter", () => {
 profileImg.addEventListener("mouseleave", () => {
   profileImg.src = originalSrc;
 });
+
+let current = 0;
+let slideInterval;
+// スライド切り替え
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle("active", i === index);
+  });
+}
+
+// 次へ
+function nextSlide() {
+  current = (current + 1) % slides.length;
+  showSlide(current);
+}
+
+// 前へ
+function prevSlide() {
+  current = (current - 1 + slides.length) % slides.length;
+  showSlide(current);
+}
+
+// 自動再生
+function startAutoPlay() {
+  slideInterval = setInterval(nextSlide, 4000); // 4秒ごとに切り替え
+}
+
+function stopAutoPlay() {
+  clearInterval(slideInterval);
+}
+
+// イベント設定
+next.addEventListener("click", () => {
+  nextSlide();
+  stopAutoPlay();
+  startAutoPlay();
+});
+
+prev.addEventListener("click", () => {
+  prevSlide();
+  stopAutoPlay();
+  startAutoPlay();
+});
+
+// 最初のスライドと自動再生開始
+showSlide(current);
+startAutoPlay();
